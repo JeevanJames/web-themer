f31f03876ff0910c069fd896b68a463999597675
Jeevan James <jeevanjj@live.com>
2017-07-26 20:11:55 +0530

    Added watcher capabilities and more command line options to build and watch commands.

--- cli.js
+++ cli.js
@@ -10,27 +10,33 @@ const _ = require('lodash');
 
 const helpCommand = require(`./lib/command/help`);
 
-function displayHelp() {
-    helpCommand.run({ '_': [] });
+function displayHelp(command) {
+    helpCommand.run({ '_': command ? [command] : [] });
 }
 
 let allArgs = process.argv;
+
+//Check if we are executed using Node.exe
+//Only on Windows and needed for my debugging purposes.
 let runningFromNode = _.endsWith(allArgs[0].toLowerCase(), 'node.exe');
 
+//Verify the number of args passed. There should be at least a command specified.
 let argsCount = runningFromNode ? allArgs.length - 3 : allArgs.length - 2;
 if (argsCount < 0) {
     displayHelp();
     return -1;
 }
 
+//Process the args after the command.
 let argv = minimist(allArgs.slice(runningFromNode ? 3 : 2));
 
+//Try to load the command module.
 let command = runningFromNode ? allArgs[2] : allArgs[1];
 let commandModule;
 try {
     commandModule = require(`./lib/command/${command}`);
 } catch (ex) {
-    console.log(chalk.red(`Invalid command: '${command}'.`));
+    console.log(chalk.red(`Unrecognized command: '${command}'.`));
     if (argv.verbose) {
         console.log(chalk.white.bgRed(ex));
     }
@@ -38,13 +44,15 @@ try {
     return -1;
 }
 
+//Command-specific validation of the args
 if (commandModule.processArgs) {
     try {
         commandModule.processArgs(argv);
     } catch (err) {
         console.log(chalk.red(err));
-        helpCommand.run({ '_': [command] });
+        displayHelp(command);
     }
 }
 
+//Execute the module
 commandModule.run(argv);
